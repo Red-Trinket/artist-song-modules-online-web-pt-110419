830139ede8a4874e30609a943ad3b40e111ccb25
Matthew Jones <matts3371@gmail.com>
2019-12-16 18:13:08 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,33 @@
-module Memorable
-  
-  def reset_all
-    self.all.clear
+module Paramable  
+  module InstanceMethods
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
   end
+end
+# //////////////////////////////////////////
+module Findable
+  module ClassMethods
+    def find_by_name(name)
+      self.all.detect{|o| o.name}
+    end    
+  end
+end
+# //////////////////////////////////////////
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
-  end 
+    def count
+      self.all.count
+    end
+  end
   
-end 
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
